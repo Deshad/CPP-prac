eedc2c263c9e48cb7fd73447db91e4f3bed0d1be
Deshad <gagan.deshad@gmail.com>
2025-01-16 14:52:04 +0000

    char data type

---
+++ intro_c++.cpp
@@ -0,0 +1,12 @@
+// intro_c++.cpp
+#include <iostream>
+
+using std::cout;
+
+int main() {
+    int slices;//initialization of the variable
+    slices=4+2;//
+    cout<<slices<<std::endl;
+
+    
+}
