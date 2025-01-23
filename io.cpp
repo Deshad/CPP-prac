b8f96fb321abbc7f8571d68dfbbd1fb5e0094905
Deshad <gagan.deshad@gmail.com>
2025-01-23 10:47:18 +0000

    char vector

--- io.cpp
+++ io.cpp
@@ -1,5 +1,17 @@
 #include <fstream>
+#include <iostream>
+#include <vector>
 int main()
 {
-    
+    std::ifstream file("text.txt");
+    std::vector<char> lines;//changing to char vector
+
+    char input;
+    while(file >> input){
+        lines.push_back(input);
+    }
+    for (char line : lines){
+        std::cout<<line<<"\n";
+    }
+    return 0;
 }
