5106a86b1bce4db9f53b487b16bfc3063aa8c54c
Deshad <gagan.deshad@gmail.com>
2025-01-26 10:39:29 +0000

    static

--- oop/main.cpp
+++ oop/main.cpp
@@ -192,5 +192,74 @@ int main()
 }
 
 */
+class User 
+{   static int user_count; //static int user_count=0  x;//can assign static value inline
+    
+    std::string status="Active";
+    
+    public:
+        static int get_user_count()
+        {
+            return user_count;//returning static data member
+        }
+        std::string first_name;//this is a data member of class User
+        std::string last_name;
+        std::string get_status()//this is a getter
+        {
+            return status;
+        }
+        void set_status(std::string status)//this is a setter
+        {
+            if (status=="active" || status=="inactive")
+            {
+                this->status=status;
+
+            }
+            else {
+            {
+                std::cout<<"Invalid status. Status should be either 'active' or 'inactive'.";
+            }
+            }
+            
+        }
+
+    User()
+    {
+        user_count++;//incrementing static data member
+        
+    }
+    User(std::string first_name, std::string last_name,std::string status)
+    {
+        this->first_name=first_name;
+        this->last_name=last_name;
+        this->status=status;
+        user_count++;//incrementing static data member
+    }
+    ~User()//creating a destructor for class User
+    {
+        user_count--;//decrementing static data member
+    }
+};
+int User::user_count=0;//here we initialize static data member
+int add_user_if_not_exist(std::vector<User> &users,User user)
+{
+    for(int i=0;i<users.size();i++)
+    {
+        if(users[i].first_name==user.first_name &&
+            users[i].last_name==user.last_name)
+        {
+            return i;
+        }
+    }
+    users.push_back(user);
+    return users.size()-1;
+}
+
+int main() 
+{
+   User user,user1,user2;//creating users
+   std::cout<<User::get_user_count()<<std::endl;
+}
+
 
 
